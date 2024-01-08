48f47b8a28abe6a20c6f5a8f840d7c3a13563e12
Mohamed Willy <mohamedwilly2001@gmail.com>
2024-01-08 23:51:01 +0200

    Finishing Xylphone

--- xylphone/lib/main.dart
+++ xylphone/lib/main.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void playSound(int index) {
+    final audio = AudioPlayer();
+    audio.play(AssetSource("note ($index).mp3"));
+  }
+
+  Widget customButton(Color c, int index) {
+    return Expanded(
+        child: Container(
+      color: c,
+      child: TextButton(
+        onPressed: () {
+          playSound(index);
+        },
+        style: TextButton.styleFrom(
+          minimumSize: Size.infinite,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        ),
+        child: const Text(""),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-
+        body: SafeArea(
+          child: Column(
+            children: [
+              customButton(Colors.red, 1),
+              customButton(Colors.orange, 2),
+              customButton(Colors.yellow, 3),
+              customButton(Colors.green, 4),
+              customButton(Colors.blue, 5),
+              customButton(Colors.indigo, 6),
+              customButton(Colors.purple, 7),
+            ],
+          ),
+        ),
       ),
     );
   }
