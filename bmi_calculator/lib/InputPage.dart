b08bedbc5e022c34b75bf386d0bdbfde4dc3251c
Mohamed Willy <mohamedwilly2001@gmail.com>
2024-01-13 13:25:18 +0200

    Adding BMI Calculator

---
+++ bmi_calculator/lib/InputPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key, required this.title});
+  final String title;
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BMI CALCULATOR"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ bmi_calculator/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'InputPage.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      // ThemeData Documentation: https://api.flutter.dev/flutter/material/ThemeData-class.html
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF0A0E21),
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
+      home: const InputPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
